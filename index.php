09190b0c2da9828ec0c99833ab0c02f051ed919b
donaji13 <toltekochoa12@gmail.com>
2015-04-15 12:26:06 -0700

    finished video 4 created a new file called connect.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> Donaji's To-Do List </title>
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+
+<body>
+
+
+	<div class="wrap">
+	
+	</div>
+
+
+</body>
+
+
+
+
+</html>
