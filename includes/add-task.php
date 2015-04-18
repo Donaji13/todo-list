2c3939b8066d378c93b23168ec8c56dbe4b1e03d
donaji13 <toltekochoa12@gmail.com>
2015-04-18 09:36:34 -0700

    finished video 12

--- includes/add-task.php
+++ includes/add-task.php
@@ -10,4 +10,15 @@
 
  	$query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
 
+ 	if ($result = $mysqli->query($quey)) {
+ 		while ($row = $result->fetch_assoc()) {
+ 			$task_id = $row['id'];
+ 			$task_name = $row['task'];
+ 		}
+ 	}
+
+
+
+ 	echo '<li><span>'.$task_name.'</span><img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" /></li>';
+
 ?>
